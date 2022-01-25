045a6138d563ab445a9351acb2d8eb96f633e6da
rubyowo <perhaps-you-know@what-is.ml>
2022-01-25 13:05:57 +0400

    Update aAAAaAAaaaAaaAAaAaAAaAaaAaAaaAAaAaaaaAAaaAaaaaAaAaaaAAaAAAaaaaAAAAAAaAaAAaaAaaAaaaAaAaaaaaAaAaaaaAaa.js

--- aAAAaAAaaaAaaAAaAaAAaAaaAaAaaAAaAaaaaAAaaAaaaaAaAaaaAAaAAAaaaaAAAAAAaAaAAaaAaaAaaaAaAaaaaaAaAaaaaAaa.js
+++ aAAAaAAaaaAaaAAaAaAAaAaaAaAaaAAaAaaaaAAaaAaaaaAaAaaaAAaAAAaaaaAAAAAAaAaAAaaAaaAaaaAaAaaaaaAaAaaaaAaa.js
@@ -1,9 +1,11 @@
-function letter() {
+function letter(repeat) {
     let str = "";
-    for (let i = 0; i<26; i++) {
+    for (let i = 0; i<repeat; i++) {
         str += Math.random() > 0.5 ? "a".toLowerCase() : "a".toUpperCase()
     }
     return str 
 }
 
-for (let j = 0; j<26; j++) {console.log(letter())}
+const a = document.getElementById("a")
+for (let k = 0; k<50; k++) {a.innerText = letter(50)}
+for (let j = 0; j<26; j++) {console.log(letter(26))}
