2ea07435125864a317b6e2e098c8730ef5e31860
rubyowo <perhaps-you-know@what-is.ml>
2022-01-25 11:59:10 +0400

    Update aAAAaAAaaaAaaAAaAaAAaAaaAaAaaAAaAaaaaAAaaAaaaaAaAaaaAAaAAAaaaaAAAAAAaAaAAaaAaaAaaaAaAaaaaaAaAaaaaAaa.js

--- aAAAaAAaaaAaaAAaAaAAaAaaAaAaaAAaAaaaaAAaaAaaaaAaAaaaAAaAAAaaaaAAAAAAaAaAAaaAaaAaaaAaAaaaaaAaAaaaaAaa.js
+++ aAAAaAAaaaAaaAAaAaAAaAaaAaAaaAAaAaaaaAAaaAaaaaAaAaaaAAaAAAaaaaAAAAAAaAaAAaaAaaAaaaAaAaaaaaAaAaaaaAaa.js
@@ -1,1 +1,10 @@
-while ;; console.log("a")
+function letter(letter, repeat) {
+    let str = "";
+    for (let i = 0; i < repeat; i++) {
+        str += Math.random() > 0.5 ? letter.toLowerCase() : letter.toUpperCase()
+    }
+    return str 
+}
+
+let a = 0
+while;; a++; letter("a", a)
